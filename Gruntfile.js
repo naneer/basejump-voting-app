3413c9550c2d4e64558f5b0cffe663b0434ae983
reenan <reenan.arbitrario@gmail.com>
2015-06-05 01:46:14 +0000

    initial commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2015-06-04 using generator-angular-fullstack 2.0.13
+// Generated on 2015-06-05 using generator-angular-fullstack 2.0.13
 'use strict';
 
 module.exports = function (grunt) {
